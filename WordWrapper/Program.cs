3858ca7ed3f54ab129626140091cad45cc85e30f
Haven White <havenw2000@hotmail.com>
2019-11-27 20:50:48 -0700

    Added commands to get text input from user and initial function to add line breaks at desired page length

--- WordWrapper/Program.cs
+++ WordWrapper/Program.cs
@@ -13,17 +13,28 @@ namespace ConsoleApp1
         {
             //Declare variables
             string outputText = text;
-            
+            int textLength = text.Length;
+            int insertLocation = maxLineLength;
+
             //Create a loop that goes to desired character determined by maxLineLength unless it reaches end line
+
 
             //Determine if character is a space
 
+
             //If character is not a space then loop back until it reaches a space
 
+
             //If/when character is a space then insert line break
+            while(insertLocation < textLength)
+            {
+                outputText = outputText.Insert(insertLocation, "\n");
+                insertLocation = insertLocation + maxLineLength;
+            }
 
+            WriteLine(outputText);
 
-            return "";
+            return outputText;
         }
     }
 
@@ -35,22 +46,39 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //Declare variables
-            string userDeclaredText = "";
+            string userInput = "";
             int userDeclaredMaxLength = 0;
             
             //Ask for line length 
-            WriteLine("How many characters long would you like your page length to be?");
+            WriteLine("Enter in desired page length in number of characters per line (must be a whole number higher than 0).");
 
             //Get desired page length
-            userDeclaredMaxLength = Console.ReadLine();
+            while(userDeclaredMaxLength <= 0)
+            {
+                userInput = ReadLine();
 
-
+                if (int.TryParse(userInput, out int number))
+                {
+                    userDeclaredMaxLength = Convert.ToInt32(userInput);
+                    if(userDeclaredMaxLength <= 0)
+                    {
+                        Console.WriteLine($"{userInput} is not a whole number above 0, enter in a whole number.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{userInput} is not a whole number above 0, enter in a whole number higher than 0.");
+                }
+            }
+            
             //Ask for text to be put in desired page length
+            WriteLine("\nEnter the text you wish to fit the desired page length.");
 
             //Get text entered in
+            userInput = ReadLine();
 
             //Pass desired length and text to Wrap function
-
+            WordWrapper.Wrap(userInput, userDeclaredMaxLength);
 
             ReadKey();
         }
